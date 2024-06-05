e6120297348adfb9ea83eea0acf1e39eeaa367a3
marvolo-riddle <iovcha@icloud.com>
2024-06-05 14:22:27 +0300

    67

--- js/script.js
+++ js/script.js
@@ -1,66 +1,89 @@
 'use strict';
-const toDo = 'todo-items';
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const button = document.querySelector('.btn-delete');
-const tbody = document.querySelector('tbody');
-let currentID = 1;
+(function () {
+  const toDo = 'todo-items';
+  const form = document.querySelector('form');
+  const input = document.querySelector('input');
+  const button = document.querySelector('.btn-delete');
+  const tbody = document.querySelector('tbody');
 
 
-const data = {
-  id: null,
-  text: null
-};
-const getData = () => {
-  const data = JSON.parse(localStorage.getItem(toDo));
-  return data ? data : [];
-}
-const createTodoItem = () => {
-  event.stopPropagation();
-  event.preventDefault();
-  if (input) {
-    const dataToSave = structuredClone(data);
+  const getData = () => {
+    const data = JSON.parse(localStorage.getItem(toDo));
+    return data ? data : [];
+  }
+
+  let currentID = (() => {
     const savedData = getData();
-    dataToSave[input.type] = input.value;
-    dataToSave.id = currentID;
+    return savedData.length ? Math.max(...savedData.map(item => item.id)) + 1 : 1;
+  })();
+
+  const createTodoItem = (event) => {
+    event.preventDefault();
+    if (!input.value.trim()) {
+      return;
+    }
+
+    const dataToSave = {
+      id: currentID,
+      text: input.value
+    };
     currentID++;
+    const savedData = getData();
     savedData.push(dataToSave);
     localStorage.setItem(toDo, JSON.stringify(savedData));
 
-    createElement(dataToSave); // Передаем обновленные данные в функцию createElement
+    createElement(dataToSave);
+    input.value = '';
   }
-}
-const createElement = (data) => {
-  event.preventDefault();
-  event.stopPropagation();
 
-  if (data) {
+  const createElement = (data) => {
+    if (!data) {
+      return;
+    }
+
     const todoItem = document.createElement('tr');
     todoItem.classList.add('todo-item');
-    todoItem.innerHTML = `<td>#${data.id} | ${data.text}</td>
-     <td>
-     <button type="submit" class="btn btn-danger btns">
-      Delete
-      </button>
-</td>`
-    tbody.append(todoItem)
-
+    todoItem.innerHTML = `
+      <td>#${data.id} | ${data.text}</td>
+      <td>
+        <button type="button" class="btn btn-danger btns">
+          Delete
+        </button>
+      </td>
+    `;
+    tbody.append(todoItem);
 
     const buttonToDelete = todoItem.querySelector('.btns');
-    const deleteTodo = () => {
-      event.stopPropagation()
-      todoItem.remove()
-    }
-    buttonToDelete.addEventListener('click', deleteTodo)
-
+    const deleteTodo = (event) => {
+      event.stopPropagation();
+      todoItem.remove();
+      const updatedData = getData().filter(item => item.id !== data.id);
+      localStorage.setItem(toDo, JSON.stringify(updatedData));
+    };
+    buttonToDelete.addEventListener('click', deleteTodo);
   }
-}
-const deleteAllToDo = () => {
-  event.stopPropagation();
-  tbody.remove()
-  localStorage.clear()
-}
+
+  const deleteAllToDo = (event) => {
+    event.stopPropagation();
+    tbody.innerHTML = '';
+    localStorage.removeItem(toDo);
+  }
+
+  const loadTodos = () => {
+    const savedData = getData();
+    savedData.forEach(createElement);
+  }
+
+  form.addEventListener('submit', createTodoItem);
+  button.addEventListener('click', deleteAllToDo);
+
+  loadTodos();
+
+})();
 
 
-form.addEventListener('submit', createTodoItem);
-button.addEventListener('click', deleteAllToDo);
+
+
+
+
+
