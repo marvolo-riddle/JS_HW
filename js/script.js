40bfb403970db23812971f12120d11e44360d58b
marvolo-riddle <iovcha@icloud.com>
2024-06-09 15:50:52 +0300

    68

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
 'use strict';
+(function () {
+  const iterator = function (itrObject) {
+
+    const myArray = Array.from(itrObject);
+    let currentIndex = 0;
+
+
+    return {
+      next() {
+        let done = false;
+        if (currentIndex + 1 > myArray.length) {
+          done = true;
+        }
+        const result = {
+          value: myArray[currentIndex],
+          done
+        }
+        currentIndex += 1;
+        return result;
+
+      }
+    }
+  }
+
+  const arr = ['1', '2', '3', '4'];
+
+  const firstObj = iterator(arr);
+  const secondObj = iterator(arr);
+
+  console.log(firstObj.next())
+  console.log(firstObj.next())
+
+  console.log('------------------')
+
+  console.log(secondObj.next())
+  console.log(secondObj.next())
+  console.log(secondObj.next())
+  console.log(secondObj.next())
+  console.log(secondObj.next())
+
+  console.log('------------------')
+
+  console.log(firstObj.next())
+  console.log(firstObj.next())
+  console.log(firstObj.next())
+  
+})();
